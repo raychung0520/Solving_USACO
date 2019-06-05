d4003d788f3a2b5308de187dc67953e316a677de
raychung0520 <raychung0520@gmail.com>
2019-06-04 18:11:29 -0700

    gift1 problem created
    Start to solve gift1 problem

--- src/_1_2/gift1.java
+++ src/_1_2/gift1.java
@@ -1,10 +1,51 @@
 package _1_2;
 
+import java.util.*;
+import java.io.*;
 public class gift1 {
 
+	//
+	static Scanner in;
+	static PrintWriter out;
+	static String inFile = "gift.in";
+	static String outFile = "gift.out";
+
+	static String result;
+
+	// Input Variables
+
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		try {
+			
+			in = new Scanner(new File(inFile));
+			out = new PrintWriter(new File(outFile));
+			
+			init();
+			out.println(solve());
+			
+			in.close();
+			out.close();
+			
+			
+		}catch(Exception e){
+			System.out.println(e);
+		}
 
 	}
+	
+	private static void init() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private static String solve() {
+
+		return result;
+	}
+	
+	//Algorithm Methods
+
+	
 
 }
