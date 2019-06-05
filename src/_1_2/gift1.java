81d9afaa33b6f625039432c7273e81e97c3ed295
raychung0520 <raychung0520@gmail.com>
2019-06-05 13:34:29 -0700

    Solved gift1 problem
    Solved

--- src/_1_2/gift1.java
+++ src/_1_2/gift1.java
@@ -1,18 +1,26 @@
-package _1_2;
-
+//package _1_2;
+/*
+ID: raychun2
+LANG: JAVA
+TASK: gift1
+*/
 import java.util.*;
+import java.util.Map.Entry;
 import java.io.*;
+
 public class gift1 {
 
-	//
+	
 	static Scanner in;
 	static PrintWriter out;
-	static String inFile = "gift.in";
-	static String outFile = "gift.out";
+	static String inFile = "gift1.in";
+	static String outFile = "gift1.out";
 
-	static String result;
+	static String result = "";
 
 	// Input Variables
+	
+	static LinkedHashMap<String, Integer> list;
 
 	public static void main(String[] args) {
 		
@@ -35,7 +43,52 @@ public class gift1 {
 	}
 	
 	private static void init() {
-		// TODO Auto-generated method stub
+		int NP = Integer.parseInt(in.nextLine());
+		list = new LinkedHashMap<String, Integer>();
+		for(int i = 0; i < NP; i++) {
+			list.put(in.nextLine(), 0);
+		}
+		
+		for(int i = 0; i < NP; i++) {
+			String giver = in.nextLine();
+			String [] temp = in.nextLine().split(" ");
+			int totalMoney = Integer.parseInt(temp[0]);
+			int numbPeop = Integer.parseInt(temp[1]);
+			
+			if(numbPeop == 0) continue; 
+			
+			int money = totalMoney / numbPeop;
+			int left = totalMoney - (money * numbPeop);
+			
+			list.put(giver, list.get(giver)-totalMoney + left);
+			
+			for(int j = 0; j < numbPeop; j++) {
+				String receiver = in.nextLine();
+				list.put(receiver, list.get(receiver)+money);
+			}
+		}
+		
+		Iterator it = list.keySet().iterator();
+		while(it.hasNext()) {
+			String key = it.next().toString();
+			int value = list.get(key);
+			
+			result += key + " " + value;
+			
+			if(it.hasNext())
+				result += "\n";
+		}
+		
+//		list.forEach((k,v) -> {
+//		    System.out.println(k + " " + v);
+//		});
+		
+//		for(Entry<String, Integer> list: list.entrySet()) {
+//			String name = list.getKey();
+//			int money = list.getValue();
+//			
+//			result += name + " " + money + "\n";
+//		}
 		
 	}
 
